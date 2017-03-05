fe4779e6a8d4a0a6e268e68956c272a16646c1b0
officekanev <office_kanev@mail.bg>
2017-03-05 17:44:24 +0200

    website

---
+++ interior_desighn_website_in_proces_build/Scripts/printNavigations.js
@@ -0,0 +1,141 @@
+function printNavigation() {
+    let html = '<div id="SITE_CONTAINER">\n';
+    html += '   <aside class="social">\n';
+    html += '       <a href="https://www.facebook.com/PK-Interior-Design-1581063942204061/?ref=aymt_homepage_panel"><img src="../images/face-book-icon-petya.png" alt="socialNetwork"></a>\n';
+    html += '   </aside>\n';
+    html += '       <div class="SITE_ROOT">\n';
+    html += '           <section id="header-text-decoration">\n';
+    html += '               <div  class="s1" >\n';
+    html += '                   <section class="logo">\n';
+    html += '                       <img src="../images/petya-logo.jpg" alt="">\n';
+    html += '                   </section>\n';
+    html += '                   <section class="logo" id="logo">\n';
+    html += '                       <div class="text-befor-decoration">\n';
+    html += '                           <h1 class="font_0">\n';
+    html += '                               <span>PK &#8203;&#8203;&#8203;&#8203;&#8203;&#8203;&#8203;Interior Design</span>\n';
+    html += '                           </h1>\n';
+    html += '                       </div>\n';
+    html += '                       <div  id="text-decoration">\n';
+    html += '                           <img src="../images/petya-dekoraciq.png" alt="">\n';
+    html += '                       </div>\n';
+    html += '                       <div class="text-after-decoration">\n';
+    html += '                           <h1 class="font_0"><span> Business Development</span></h1>\n';
+    html += '                       </div>\n';
+    html += '                   </section>\n';
+    html += '               </div>\n';
+    html += '           </section>\n';
+    html += '           <div class="navigation">\n';
+    html += '               <nav>\n';
+    html += '                   <ul class="parent-navigation">\n';
+    html += '                       <li><a href="home.html">Home</a></li>\n';
+    html += '                       <li>\n';
+    html += '                           Portfotio\n';
+    html += '                           <ul class="drop-down" id="about-menu">\n';
+    html += '                               <li><a href="../pages/residential.html"><strong>Residential</strong></a></li>\n';
+    html += '                               <li><a href="../pages/hospitality.html"><strong>Hospitality</strong></a></li>\n';
+    html += '                               <li><a href="../pages/businessdevelopment.html"><strong>Business Development</strong></a></li>\n';
+    html += '                               <li><a href="../pages/businessdevelopment.html"><strong>Business Development</strong></a></li>\n';
+
+    return html;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
